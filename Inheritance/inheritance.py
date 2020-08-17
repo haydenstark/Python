697678bddfb492684826a973de51d5be1a50179b
Hayden Stark <haydenstark@aol.com>
2020-08-17 12:07:26 -0500

    Add Inheritance

---
+++ Inheritance/inheritance.py
@@ -0,0 +1,18 @@
+
+
+class Condo:
+    name = 'Not Provided'
+    address = ' '
+
+class Unit(Condo):
+    unitNumber = 101
+    sqft = 750
+    parking = True
+    parkingSpot = 1
+
+class Laundry(Condo):
+    floor = 2
+    numberOfWashers = 8
+    numberOfDryers = 10
+    hours = '9am-9pm'
+    
