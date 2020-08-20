6fe08873761f919a48a9316cf6fd2ab7350c0807
Hayden Stark <haydenstark@aol.com>
2020-08-20 16:30:09 -0500

    Fix private/protected

--- private_protected.py
+++ private_protected.py
@@ -4,5 +4,17 @@ class Secure:
     def setProtected(self, protected):
         self._protectedVar = protected
 
+    def getPrivate(self):
+        print(self.__privateVar)
+
     def setPrivate(self, private):
         self.__privateVar = private
+
+
+x = Secure()
+x.setProtected(21)
+x.setPrivate(7)
+
+
+print(x._protectedVar)
+x.getPrivate()
